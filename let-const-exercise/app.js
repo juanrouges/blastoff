d6752429b9edeec2624a5aa2de8ea3f3a04a16b6
Juan Rojas <juanrojas1912@gmail.com>
2022-10-05 21:57:16 -0400

    let-const-exercise completed

---
+++ let-const-exercise/app.js
@@ -0,0 +1,36 @@
+// 1. stop me from doing this!
+// var PI = 3.14;
+// PI = 42;
+
+const PI = 3.14;
+PI = 42 // This will trhwo an error can't be re-assign
+
+// ****************************************************
+// Quiz
+// ****************************************************
+
+// What is the difference between var and let?
+// A: Js 'var' is a legacy declaration that allow us to
+// initialize variables. var can changed its value at any
+// point and can also be redeclared. 'let'was part of a 
+// series of updates done to js in its ES2015 version. let 
+// just like var, allow us to initilize varibales - its 
+// value can change but it can't be redeclared. it is blocked
+// scoped as well, unlive var. 
+
+// What is the difference between var and const?
+// A: Js 'var' is a legacy declaration that allow us to
+// initialize variables. var can changed its value at any
+// point and can also be redeclared. 'const'was part of a 
+// series of updates done to js in its ES2015 version. const 
+// just like var, allow us to initilize varibales - its 
+// value can't change and it can't be redeclared. it is blocked
+// scoped as well, unlive var. 
+
+// What is the difference between let and const?
+// 'let' and 'const' work very similar but const is more strict
+// as its inilized valu can't be changed. let does allow it.
+
+// What is hoisting?
+// var declaration are hoisted onload, meaning variables are 
+// created but its values are assined later on.
