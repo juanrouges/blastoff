e92caac431d0ab9ddc7b95ad1223ff2070f44084
Juan Rojas <juanrojas1912@gmail.com>
2022-10-06 11:26:16 -0400

    arrow-function-exercises completed

--- arrow-functions-exercise/app.js
+++ arrow-functions-exercise/app.js
@@ -1,8 +1,27 @@
-function double(arr) {
-  return arr.map(function(val) {
-    return val * 2;
-  });
-}
-
-/* Write an ES2015 Version */
+// 1. Refactor the above code to use two arrow functions. Turn it into a one-liner.
+// function double(arr) {
+//   return arr.map(function (val) {
+//     return val * 2;
+//   });
+// }
+// My answer
 const double = (arr) => arr.map((val) => val * 2);
+
+// 2. Replace ALL functions with arrow functions:
+const series = [2, 3, 4, 6, 8, 9, 12, 45, 78, 89, 95, 145, 678];
+// function squareAndFindEvens(numbers) {
+//   var squares = numbers.map(function (num) {
+//     return num ** 2;
+//   });
+//   var evens = squares.filter(function (square) {
+//     return square % 2 === 0;
+//   });
+//   return evens;
+// }
+// My answer
+const squareAndFindEvens = (numbers) => {
+  const squares = numbers.map((num) => num ** 2);
+  const evens = squares.filter((square) => square % 2 === 0);
+  return evens;
+};
+console.log(squareAndFindEvens(series));
