f99fb6469cafba431f6b9541601be123cccbe2d7
Juan Rojas <juanrojas1912@gmail.com>
2022-10-06 20:45:34 -0400

    object enhancements exercises init

---
+++ object-enhancements-exercise/app.js
@@ -0,0 +1,18 @@
+// 1.
+
+// In this exercise, you’ll refactor some ES5 code into ES2015. 
+// Write your code in the sections with a comment to “Write an 
+// ES2015 Version”.
+
+// Same keys and values
+
+// function createInstructor(firstName, lastName){
+//   return {
+//     firstName: firstName,
+//     lastName: lastName
+//   }
+// }
+
+// Write an ES2015 Version
+
+// A: 
