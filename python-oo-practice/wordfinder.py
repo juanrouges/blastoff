4b1c14d028b746dacfd4810bcebfc1fa11731a80
Juan Rojas <juanrojas1912@gmail.com>
2022-11-03 16:16:43 -0400

    python-oo-practice wordfinder first part complted

--- python-oo-practice/wordfinder.py
+++ python-oo-practice/wordfinder.py
@@ -1,5 +1,20 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
+
 
 class WordFinder:
-    ...
+    def __init__(self, file):
+        self.file = file
+        self.words = []
+        self.load()
+
+    def load(self):
+        items = open(self.file, 'r')
+        for item in items:
+            self.words.append(item)
+
+        print(f"{len(self.words)} words read")
+
+    def random(self):
+        return random.choice(self.words)
