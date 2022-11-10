34b6f7a34728dfe974daf2cf24b45e219609e578
Juan Rojas <juanrojas1912@gmail.com>
2022-11-10 11:07:33 -0500

    flask-survey render satisfction qustiond choices

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
+from surveys import satisfaction_survey
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "this-are-the-irulis"
@@ -10,4 +12,8 @@ debug = DebugToolbarExtension(app)
 
 @app.route("/")
 def renderHome():
-  return render_template("index.html")
+  title = satisfaction_survey.title
+  instructions = satisfaction_survey.instructions
+  questions = satisfaction_survey.questions
+
+  return render_template("index.html", title=title, instructions=instructions, questions=questions)
