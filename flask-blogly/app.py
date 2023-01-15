5837129c7f1143fa86ae4a37d7047c42b2f864ad
Juan Rojas <juanrojas1912@gmail.com>
2023-01-15 18:19:23 -0500

    flask-blogly user model.py

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,12 +1,19 @@
-"""Blogly application."""
-
-from flask import Flask
+from flask import Flask, render_template
 from models import db, connect_db
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+
+app.config['SECRET_KEY'] = 'chicharronesconcalzones123'
+# ================== Whar are this for? =======================
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+# =============================================================
 
 connect_db(app)
-db.create_all()
+
+@app.route('/')
+def home_page():
+  return render_template('root.html')
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -1,1 +1,15 @@
-"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+  db.app = app
+  db.init_app(app)
+
+class User(db.Model):
+  __tablename__ = "users"
+
+  id = db.column(db.Integer, primary_key=True, autoincrement=True)
+  first_name = db.column(db.String(20), nullable=False)
+  last_name = db.column(db.String(20), nullable=False)
+  image_url = db.column(db.String(100), default="https://placehold.co/600x400")
