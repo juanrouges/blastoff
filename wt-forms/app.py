2abdc9031216c3c5ed881c1f17d3b140bdbb263c
Juan Rojas <juanrojas1912@gmail.com>
2023-01-22 17:58:17 -0500

    wt forms unfolding flask app

--- wt-forms/app.py
+++ wt-forms/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from models import db, connect_db
+from forms import AddSnackForm
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SECRET_KEY"] = "unelefantesebalanseaba5687"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = True
 app.config["SQLALCHEMY_ECHO"] = False
 
 connect_db(app)
@@ -15,3 +16,15 @@ connect_db(app)
 @app.route("/")
 def home_page():
   return render_template("/index.html")
+
+@app.route("/add-snack", methods=["GET", "POST"])
+def add_snack():
+  form = AddSnackForm()
+
+  if form.validate_on_submit():
+    name = form.name.data
+    price = form.price.data
+    flash(f"Added {name} at ${price}")
+    return redirect("/add-snack")
+  else:
+    return render_template("snack-form.html", form=form)
